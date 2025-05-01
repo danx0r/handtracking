85dc10d1071394ffa9b953957ae06ece0eb953d8
danx0r <danbmil99@gmail.com>
2025-05-01 13:17:26 -0700

    somewhere

--- ik2.py
+++ ik2.py
@@ -385,7 +385,7 @@ class WorkspaceVisualizer:
                         # Generate random position and orientation
                         # random_pos = self.get_random_position()
                         # random_rot = self.get_random_rotation()
-                        random_pos = np.array([0.3, 0, 0])
+                        random_pos = np.array([0, 0, 0])
                         random_rot = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
                         current_target = random_pos
                         current_rot = random_rot
